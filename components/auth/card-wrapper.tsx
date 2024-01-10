14c489377612245552417c59c9faacc48634d0a3
Trivial-lore <triviallore@gmail.com>
2024-01-10 16:30:05 -0600

    finished, everything is working

--- components/auth/card-wrapper.tsx
+++ components/auth/card-wrapper.tsx
@@ -6,11 +6,10 @@ import {
   CardFooter,
   CardHeader
 } from "@/components/ui/card";
-import { Header } from "./header";
-import { Social } from "./social";
+
 import { BackButton } from "./back-button";
-// import { Header } from "@/components/auth/header";
-// import { Social } from "@/components/auth/social";
+import { Header } from "@/components/auth/header";
+import { Social } from "@/components/auth/social";
 
 
 interface CardWrapperProps {
